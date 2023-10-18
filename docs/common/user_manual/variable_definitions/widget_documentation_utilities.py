f6f7cc8cbb877da137bf4a89801d1a233bb8504d
bsaakash <11618528+bsaakash@users.noreply.github.com>
2023-10-18 14:12:18 -0700

    abs - improving formatting of code, adding remaining type hints

--- docs/common/user_manual/variable_definitions/widget_documentation_utilities.py
+++ docs/common/user_manual/variable_definitions/widget_documentation_utilities.py
@@ -1,17 +1,37 @@
+import argparse
 import os
+from pathlib import Path
+from typing import NamedTuple
+
 import pandas as pd
-from typing import Iterable, Any
 
 
-def _make_page_title(title_text: str):
-    return ("".join([title_text, "\n", "="*len(title_text), "\n\n"]))
+class InputItem(NamedTuple):
+    input_item_name_in_json_file: str
+    input_item_display_name: str
+    input_item_optional: str
+    input_item_data_type: str
+    input_item_constraints: str
+    seealso_text: str
+    seealso_link: str
+    seealso_description: str
 
 
-def _make_link_target_string(widget_name: str, input_item_name_in_json_file: str):
-    return f'.. _{"".join(widget_name.strip().split())}_{"".join(input_item_name_in_json_file.strip().split())}:\n'
+def _make_page_title(title_text: str) -> str:
+    return "".join([title_text, "\n", "=" * len(title_text), "\n\n"])
 
 
-def _make_first_line_of_definition_list_item(input_item_display_name: str, input_item_optional: str, input_item_data_type: str):
+def _make_link_target_string(
+    widget_name: str, input_item_name_in_json_file: str
+):
+    return f".. _{widget_name}_{input_item_name_in_json_file}:\n"
+
+
+def _make_first_line_of_definition_list_item(
+    input_item_display_name: str,
+    input_item_optional: str,
+    input_item_data_type: str,
+):
     term_string = f"{input_item_display_name}"
     if not pd.isnull(input_item_optional):
         classifier_string = f"*{input_item_data_type}, optional*"
@@ -20,21 +40,54 @@ def _make_first_line_of_definition_list_item(input_item_display_name: str, input
     return " : ".join([term_string, classifier_string])
 
 
-def _make_following_lines_of_definition_list_item(input_item_description: str, input_item_default_value: str, input_item_constraints: str, input_item_name_in_json_file: str):
+def _make_following_lines_of_definition_list_item(
+    input_item_description: str,
+    input_item_default_value: str,
+    input_item_constraints: str,
+    input_item_name_in_json_file: str,
+):
     definition_string_list = [f"\t{input_item_description}"]
     if not pd.isnull(input_item_default_value):
         definition_string_list.append(f"Default: {input_item_default_value}")
     if not pd.isnull(input_item_constraints):
         definition_string_list.append(f"Constraints: {input_item_constraints}")
-    definition_string_list.append(f"Key in JSON file: {input_item_name_in_json_file}\n")
+    definition_string_list.append(
+        f"Key in JSON file: {input_item_name_in_json_file}\n"
+    )
     return "\n\n\t".join(definition_string_list)
 
 
-def _make_definition_list_item_from_parameter_data(widget_name: str, input_item_name_in_json_file: str, input_item_display_name: str, input_item_optional: str, input_item_data_type: str, input_item_description: str, input_item_default_value: str, input_item_constraints: str):
-    link_target_string = _make_link_target_string(widget_name, input_item_name_in_json_file)
-    first_line_of_definition_item = _make_first_line_of_definition_list_item(input_item_display_name, input_item_optional, input_item_data_type)
-    following_lines_of_definition_item = _make_following_lines_of_definition_list_item(input_item_description, input_item_default_value, input_item_constraints, input_item_name_in_json_file)
-    return "\n".join([link_target_string, first_line_of_definition_item, following_lines_of_definition_item])
+def _make_definition_list_item_from_parameter_data(
+    widget_name: str,
+    input_item_name_in_json_file: str,
+    input_item_display_name: str,
+    input_item_optional: str,
+    input_item_data_type: str,
+    input_item_description: str,
+    input_item_default_value: str,
+    input_item_constraints: str,
+):
+    link_target_string = _make_link_target_string(
+        widget_name, input_item_name_in_json_file
+    )
+    first_line_of_definition_item = _make_first_line_of_definition_list_item(
+        input_item_display_name, input_item_optional, input_item_data_type
+    )
+    following_lines_of_definition_item = (
+        _make_following_lines_of_definition_list_item(
+            input_item_description,
+            input_item_default_value,
+            input_item_constraints,
+            input_item_name_in_json_file,
+        )
+    )
+    return "\n".join(
+        [
+            link_target_string,
+            first_line_of_definition_item,
+            following_lines_of_definition_item,
+        ]
+    )
 
 
 def _make_seealso_beginning():
@@ -42,7 +95,7 @@ def _make_seealso_beginning():
     return string
 
 
-def _make_link_reference_string(text, uri):
+def _make_link_reference_string(text: str, uri: str):
     if uri.lower().startswith("http"):
         link = f"\t`{text} <{uri}>`_\n"
     else:
@@ -50,27 +103,35 @@ def _make_link_reference_string(text, uri):
     return link
 
 
-def _make_seealso(row):
+def _make_seealso(row: InputItem):
     string = _make_link_reference_string(row.seealso_text, row.seealso_link)
     if not pd.isnull(row.seealso_description):
         string += f"\t\t{row.seealso_description}\n"
     return string
 
 
-def _make_list_of_strings_for_rst_definition_list(widget_name, widget_data):
+def _make_list_of_strings_for_rst_definition_list(
+    widget_name: str, widget_data: pd.DataFrame
+):
     list_of_strings_definition_list = []
     for row in widget_data.itertuples():
         if not pd.isnull(row.name):
             list_of_strings_definition_list.append(
                 _make_definition_list_item_from_parameter_data(
-                    widget_name, row.name, 
-                    row.display_name, row.optional, 
-                    row.data_type, row.description, 
-                    row.default_value, row.constraints))
+                    widget_name,
+                    row.name,
+                    row.display_name,
+                    row.optional,
+                    row.data_type,
+                    row.description,
+                    row.default_value,
+                    row.constraints,
+                )
+            )
     return list_of_strings_definition_list
 
 
-def _make_list_of_strings_for_rst_seealso(widget_data):
+def _make_list_of_strings_for_rst_seealso(widget_data: pd.DataFrame):
     list_of_strings_seealso = []
     for row in widget_data.itertuples():
         if not pd.isnull(row.seealso_text):
@@ -78,9 +139,15 @@ def _make_list_of_strings_for_rst_seealso(widget_data):
     return list_of_strings_seealso
 
 
-def make_rst_file_for_widget(rst_file_path, widget_name, widget_data):
-    list_of_strings_definition_list = _make_list_of_strings_for_rst_definition_list(widget_name, widget_data)
-    list_of_strings_seealso = _make_list_of_strings_for_rst_seealso(widget_data)
+def make_rst_file_for_widget(
+    rst_file_path: Path, widget_name: str, widget_data: pd.DataFrame
+):
+    list_of_strings_definition_list = (
+        _make_list_of_strings_for_rst_definition_list(widget_name, widget_data)
+    )
+    list_of_strings_seealso = _make_list_of_strings_for_rst_seealso(
+        widget_data
+    )
     with open(rst_file_path, "w+") as f:
         f.write(_make_page_title(widget_name))
         f.write("\n\n".join(list_of_strings_definition_list))
@@ -89,7 +156,18 @@ def make_rst_file_for_widget(rst_file_path, widget_name, widget_data):
         f.write("\n\n\n")
 
 
-def main(csv_files_directory, rst_files_directory, all_data):
+def main(
+    csv_files_directory: Path,
+    rst_files_directory: Path,
+    toc_include_file_path: Path,
+    all_data: dict[str, pd.DataFrame],
+):
+    print(f"Walking through {csv_files_directory}")
+    print(f"Creating rst files in {rst_files_directory}")
+    print(
+        f"Building the file '{toc_include_file_path}' that appropriately includes the generated rst files"
+    )
+
     rst_file_path_list = []
     for widget_name in all_data.keys():
         widget_data = all_data[widget_name]
@@ -97,31 +175,74 @@ def main(csv_files_directory, rst_files_directory, all_data):
         widget_data.to_csv(csv_file_path)
         rst_file_path = os.path.join(rst_files_directory, f"{widget_name}.rst")
         rst_file_path_list.append(rst_file_path)
-        make_rst_file_for_widget(rst_file_path, widget_name, widget_data)
-        
-    rst_file_name = "WidgetTables.rst"
+        make_rst_file_for_widget(Path(rst_file_path), widget_name, widget_data)
+
+    rst_file_name = toc_include_file_path
     with open(rst_file_name, "w+") as f:
-        f.write(_make_page_title("User Defined Inputs"))
-        f.write("\n".join([".. toctree::",
-                        "   :maxdepth: 2",
-                        "   :caption: User Defined Inputs:",
-                        "\n"]))
+        f.write(_make_page_title("User Inputs"))
+        f.write(
+            "\n".join(
+                [
+                    ".. toctree::",
+                    "   :maxdepth: 2",
+                    "   :caption: User Inputs:",
+                    "\n",
+                ]
+            )
+        )
         for rst_file in rst_file_path_list:
             f.write(f"\n   {rst_file}")
 
 
-if __name__=="__main__":
-    spreadsheet = "WidgetParameters.xlsx"
-    all_data = pd.read_excel(spreadsheet, sheet_name=None)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate rst files documenting user inputs in SimCenter graphical user interface widgets."
+    )
+    parser.add_argument(
+        "path_to_csv_files",
+        help="directory containing csv files defining user inputs",
+        type=Path,
+    )
+    parser.add_argument(
+        "path_to_rst_files",
+        help="directory where rst files for widget documentation should be written",
+        type=Path,
+    )
+    parser.add_argument(
+        "--toc_include_file_path",
+        help="path to the file that is included in the table of contents of the SimCenter application documentation",
+        default="WidgetTables.rst",
+        type=Path,
+    )
 
-    csv_files_directory = "Widget_CSV_Files"
-    rst_files_directory = "Widget_RST_Files"
-    if not os.path.exists(csv_files_directory):
-        os.mkdir(csv_files_directory)
+    command_line_arguments = parser.parse_args()
+    csv_files_directory = command_line_arguments.path_to_csv_files.resolve()
+    rst_files_directory = command_line_arguments.path_to_rst_files.resolve()
+    toc_include_file_path = (
+        command_line_arguments.toc_include_file_path.resolve()
+    )
+
+    try:
+        csv_files_directory.is_dir()
+    except:
+        raise
+    else:
+        if not csv_files_directory.is_dir():
+            raise OSError(
+                f"Specified source directory '{csv_files_directory}' does not exist"
+            )
+
     if not os.path.exists(rst_files_directory):
         os.mkdir(rst_files_directory)
-    
-    main(csv_files_directory=csv_files_directory, rst_files_directory=rst_files_directory, all_data=all_data)
+
+    spreadsheet = "WidgetParameters.xlsx"
+    all_data = pd.read_excel(spreadsheet, sheet_name=None)
+    main(
+        csv_files_directory=csv_files_directory,
+        rst_files_directory=rst_files_directory,
+        toc_include_file_path=toc_include_file_path,
+        all_data=all_data,
+    )
 
     print(all_data.keys())
     print("Done!")
